ae1e0458bdf42b9014f601304de5db8fcb3fef10
marianafigueiras97-rgb <marianafigueiras97@gmail.com>
2026-07-13 03:49:45 +0200

    estructura por carpetas y enunciados de los ejercicios

---
+++ 02_Ejercicios_ES6+_&_asincronia/ejercicio_3.js
@@ -0,0 +1,28 @@
+// 3.1 Dado el siguiente array, devuelve un array con sus nombres
+// utilizando .map().
+// const users = [
+// 	{id: 1, name: 'Abel'},
+// 	{id:2, name: 'Julia'},
+// 	{id:3, name: 'Pedro'},
+// 	{id:4, name: 'Amanda'}
+// ];
+
+// 3.2 Dado el siguiente array, devuelve una lista que contenga los valores
+// de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que
+// empiece por 'A'.
+// const users = [
+// 	{id: 1, name: 'Abel'},
+// 	{id:2, name: 'Julia'},
+// 	{id:3, name: 'Pedro'},
+// 	{id:4, name: 'Amanda'}
+// ];
+
+// 3.3 Dado el siguiente array, devuelve una lista que contenga los valores
+// de la propiedad .name y añade al valor de .name el string ' (Visitado)'
+// cuando el valor de la propiedad isVisited = true.
+// const cities = [
+// 	{isVisited:true, name: 'Tokyo'},
+// 	{isVisited:false, name: 'Madagascar'},
+// 	{isVisited:true, name: 'Amsterdam'},
+// 	{isVisited:false, name: 'Seul'}
+// ];
---
+++ 02_Ejercicios_ES6+_&_asincronia/ejercicio_8/ejercicio_8.js
@@ -0,0 +1,9 @@
+// Accederemos a los datos de una API pública de Game Of Thrones, queremos un select con todos los nombres de los personajes para que cuando un usuario seleccione un nombre salga su imagen en el medio de la página
+
+// Para obtener los datos con los que jugar necesitaremos estudiar la documentación de la API y buscar la url necesaria para los datos que queramos, para este paso os pedimos que de verdad os esforcéis buscándola en la documentación, queremos la url que me traiga los datos de todos los personajes de GOT, sin embargo, en la slide siguiente tendréis la url directa de esos datos.
+
+// URL de la documentación (para que indaguéis): https://thronesapi.com/
+
+// Esta sería la URL final (la que deberéis utilizar para vuestra petición):
+
+// https://thronesapi.com/api/v2/Characters
