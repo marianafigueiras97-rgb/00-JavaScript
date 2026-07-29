e2e36fae16e41c3b881c9bfe88e49d95b34cc043
marianafigueiras97-rgb <marianafigueiras97@gmail.com>
2026-07-29 16:01:10 +0200

    ejercicio 2 es6

--- 02_Ejercicios_ES6+_&_asincronia/ejercicio_2.js
+++ 02_Ejercicios_ES6+_&_asincronia/ejercicio_2.js
@@ -1,19 +1,36 @@
-// 2.1 Dado el siguiente array, crea una copia usando spread operators.
-// const pointsList = [32, 54, 21, 64, 75, 43]
+//2.1 Dado el siguiente array, crea una copia usando spread operators.
+const pointsList = [32, 54, 21, 64, 75, 43];
+console.log(pointsList);
 
-// 2.2 Dado el siguiente objeto, crea una copia usando spread operators.
-// const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+const newPointsList = [...pointsList];
 
-// 2.3 Dado los siguientes arrays, crea un nuevo array juntandolos usando
-// spread operatos.
-// const pointsList = [32, 54, 21, 64, 75, 43];
-// const pointsLis2 = [54,87,99,65,32];
+console.log(newPointsList);
 
-// 2.4 Dado los siguientes objetos. Crea un nuevo objeto fusionando los dos
-// con spread operators.
-// const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
-// const toyUpdate = {lights: 'rgb', power: ['Volar like a dragon', 'MoonWalk']}
+// //2.2 Dado el siguiente objeto, crea una copia usando spread operators.
+const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+console.log(toy);
 
-// 2.5 Dado el siguiente array. Crear una copia de él eliminando la posición 2
-// pero sin editar el array inicial. De nuevo, usando spread operatos.
-// const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
+const newToy = {...toy};
+console.log(newToy);
+
+
+// //2.3 Dado los siguientes arrays, crea un nuevo array juntandolos usando spread operatos.
+const lista1 = [32, 54, 21, 64, 75, 43];
+const lista2 = [54,87,99,65,32];
+
+const ambasListas = [...lista1, ...lista2];
+console.log(ambasListas);
+
+// //2.4 Dado los siguientes objetos. Crea un nuevo objeto fusionando los dos con spread operators.
+const toy2 = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+const toyUpdate = {lights: 'rgb', power: ['Volar like a dragon', 'MoonWalk']}
+
+const finalToy = {...toy2,...toyUpdate};
+console.log(finalToy);
+
+// //2.5 Dado el siguiente array. Crear una copia de él eliminando la posición 2 pero sin editar el array inicial. De nuevo, usando spread operatos.
+const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
+
+const newColors = [...colors.slice(0,2), ...colors.slice(3)];
+
+console.log(newColors);
