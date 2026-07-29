d5b0da7251395314a8055c77b1fe61d1e839dc30
marianafigueiras97-rgb <marianafigueiras97@gmail.com>
2026-07-29 16:24:41 +0200

    ejercicios 3 metodo map()

--- 02_Ejercicios_ES6+_&_asincronia/ejercicio_3.js
+++ 02_Ejercicios_ES6+_&_asincronia/ejercicio_3.js
@@ -1,28 +1,48 @@
-// 3.1 Dado el siguiente array, devuelve un array con sus nombres
-// utilizando .map().
-// const users = [
-// 	{id: 1, name: 'Abel'},
-// 	{id:2, name: 'Julia'},
-// 	{id:3, name: 'Pedro'},
-// 	{id:4, name: 'Amanda'}
-// ];
+// 3.1 Dado el siguiente array, devuelve un array con sus nombres utilizando .map().
+const users = [
+	{id: 1, name: 'Abel'},
+	{id:2, name: 'Julia'},
+	{id:3, name: 'Pedro'},
+	{id:4, name: 'Amanda'}
+];
+
+const usersNames = users.map((user) => user.name);
+console.log(usersNames);
 
 // 3.2 Dado el siguiente array, devuelve una lista que contenga los valores
 // de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que
 // empiece por 'A'.
-// const users = [
-// 	{id: 1, name: 'Abel'},
-// 	{id:2, name: 'Julia'},
-// 	{id:3, name: 'Pedro'},
-// 	{id:4, name: 'Amanda'}
-// ];
+const users2 = [
+    {id: 1, name: 'Abel'},
+	{id:2, name: 'Julia'},
+	{id:3, name: 'Pedro'},
+	{id:4, name: 'Amanda'}
+];
+
+const anacletoUsers = users2.map((user) => {
+if (user.name.startsWith('A')){
+    return 'Anacleto'
+    }
+    return user.name;
+});
+console.log(anacletoUsers);
 
 // 3.3 Dado el siguiente array, devuelve una lista que contenga los valores
 // de la propiedad .name y añade al valor de .name el string ' (Visitado)'
 // cuando el valor de la propiedad isVisited = true.
-// const cities = [
-// 	{isVisited:true, name: 'Tokyo'},
-// 	{isVisited:false, name: 'Madagascar'},
-// 	{isVisited:true, name: 'Amsterdam'},
-// 	{isVisited:false, name: 'Seul'}
-// ];
+const cities = [
+    	{isVisited:true, name: 'Tokyo'},
+    	{isVisited:false, name: 'Madagascar'},
+    	{isVisited:true, name: 'Amsterdam'},
+    	{isVisited:false, name: 'Seul'}
+    ];
+
+const newCities = cities.map((city) => {
+if(city.isVisited === true){
+    city.name = city.name + ' (Visitado)'
+}
+return city.name;
+
+});
+
+console.log(newCities);
